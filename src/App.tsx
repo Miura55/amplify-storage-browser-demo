2a32dd966342527374c90c0df64d134753aa7194
Miura55 <acordion.piano@gmail.com>
2024-12-27 07:38:38 +0900

    ストレージブラウザ機能を追加し、UIを更新。アイコンを変更し、サインアウト機能を実装。

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import { StorageBrowser } from "./components/StorageBrowser";
+import { Button } from "@aws-amplify/ui-react";
+import { signOut } from "aws-amplify/auth";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
   }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <h1>Storage Browser Demo</h1>
+      <StorageBrowser />
+      <Button onClick={handleSignOut}>Sign Out</Button>
     </main>
   );
 }
---
+++ src/components/StorageBrowser.tsx
@@ -0,0 +1,13 @@
+import { 
+  createAmplifyAuthAdapter, 
+  createStorageBrowser 
+} from "@aws-amplify/ui-react-storage/browser";
+import "@aws-amplify/ui-react-storage/styles.css";
+import { Amplify } from "aws-amplify";
+import config from "../../amplify_outputs.json";
+
+Amplify.configure(config);
+
+export const { StorageBrowser } = createStorageBrowser({
+  config: createAmplifyAuthAdapter(),
+});
